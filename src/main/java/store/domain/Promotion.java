2dc126766ffeb14c27099fc21e57fe8e554c093a
JMJ <ghtntkdnsk@naver.com>
2024-11-11 23:19:06 +0900

    feat(Promotion) : 프로모션 정보를 가지고 있는 클래스 추가

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,4 +1,50 @@
 package store.domain;
 
+import java.time.LocalDate;
+import store.view.InputView;
+
 public class Promotion {
+    private final String name;
+    private final PromotionType promotionType;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Promotion(String name, PromotionType promotionType, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.promotionType = promotionType;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public boolean isActive(LocalDate currentDate) {
+        return !currentDate.isBefore(startDate) && !currentDate.isAfter(endDate);
+    }
+
+    public int calculateDiscount(ReceiptItem item, InputView inputView) {
+        int requiredQuantity = this.getPromotionType().getRequiredQuantity();
+        int freeQuantity = this.getPromotionType().getFreeQuantity();
+        int setSize = requiredQuantity + freeQuantity;
+
+        int applicablePromotionSets = item.getProductQuantity() / setSize;
+        int remainingQuantity = item.getProductQuantity() % setSize;
+
+        int discount = applicablePromotionSets * freeQuantity * item.getProductPrice();
+
+        if (remainingQuantity >= requiredQuantity) {
+            boolean addFreeItems = inputView.askToAddFreeItems(item.getProductName(), freeQuantity);
+            if (addFreeItems) {
+                item.addQuantity(freeQuantity);
+                discount += freeQuantity * item.getPrice();
+            }
+        }
+        return discount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PromotionType getPromotionType() {
+        return promotionType;
+    }
 }
