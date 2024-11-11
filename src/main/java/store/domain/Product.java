7021123a3746ac8a7cb50673b577b1bb92dfaedf
JMJ <ghtntkdnsk@naver.com>
2024-11-11 22:00:57 +0900

    feat(Product) : 상품 정보 출력 메서드 및 프로모션 여부 확인 메서드 추가

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -12,4 +12,31 @@ public class Product {
         this.quantity = quantity;
         this.promotionName = promotionName;
     }
+
+    public boolean hasPromotion() {
+        return !promotionName.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return formatBasicInfo() + formatQuantityInfo() + formatPromotionInfo();
+    }
+
+    private String formatBasicInfo() {
+        return "- " + name + " " + String.format("%,d", price) + "원 ";
+    }
+
+    private String formatQuantityInfo() {
+        if (quantity > 0) {
+            return String.format("%,d개 ", quantity);
+        }
+        return "재고 없음 ";
+    }
+
+    private String formatPromotionInfo() {
+        if (hasPromotion()) {
+            return promotionName;
+        }
+        return "";
+    }
 }
