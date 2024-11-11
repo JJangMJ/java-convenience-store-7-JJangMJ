50858e7f86b32ddbe25f408f9941cc1191457528
JMJ <ghtntkdnsk@naver.com>
2024-11-11 22:59:15 +0900

    feat(Cashier) : 구매 과정 처리, 상품 결제 및 할인을 계산하는 Cashier 클래스 초기 구현

---
+++ src/main/java/store/domain/Cashier.java
@@ -0,0 +1,12 @@
+package store.domain;
+
+import java.util.List;
+import store.view.InputView;
+
+public class Cashier {
+    private final List<Product> products;
+    private final InputView inputView;
+
+    public Cashier() {
+    }
+}
