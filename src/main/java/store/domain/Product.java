0edd1a45cda57ffb5c7705e591e80c7a4d70e7c9
JMJ <ghtntkdnsk@naver.com>
2024-11-11 21:42:18 +0900

    feat(Product) : 상품 정보 관리를 위한 Product 클래스 추가

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,15 @@
+package store.domain;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private int quantity;
+    private final String promotionName;
+
+    public Product(String name, int price, int quantity, String promotionName) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotionName = promotionName;
+    }
+}
