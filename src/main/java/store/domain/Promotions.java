bbfab1fa571075c7da26ea28f4f526f29f4ab01c
JMJ <ghtntkdnsk@naver.com>
2024-11-11 23:19:59 +0900

    feat(Promotions) : 프로모션들을 관리하는 클래스 추가

--- src/main/java/store/domain/Promotions.java
+++ src/main/java/store/domain/Promotions.java
@@ -1,4 +1,26 @@
 package store.domain;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Promotions {
+    private final Map<String, Promotion> promotions;
+
+    public Promotions(List<Promotion> promotionList) {
+        this.promotions = new HashMap<>();
+        for (Promotion promotion : promotionList) {
+            this.promotions.put(promotion.getName(), promotion);
+        }
+    }
+
+    // 프로모션 이름으로 프로모션을 검색하는 메서드
+    public Promotion getPromotionByName(String promotionName) {
+        Promotion promotion = promotions.get(promotionName);
+        if (promotion != null && promotion.isActive(LocalDate.now())) {
+            return promotion;
+        }
+        return null;
+    }
 }
